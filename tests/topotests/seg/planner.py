906225527c801721cc8bb669c9302bea9e23e0ff
Tyler Marriner <marriner.tyler@gmail.com>
2021-06-16 11:47:28 +1200

    SR-MPLS

--- tests/topotests/seg/planner.py
+++ tests/topotests/seg/planner.py
@@ -1,12 +1,213 @@
 import json
+import sys
+import subprocess
+from jinja2 import Template, StrictUndefined
+from pathlib import Path
+import ipaddress
+import pprint
+
+from datetime import datetime
+
+topology = {
+    "routers":{},
+    "networks":{},
+}
+
+bgp_ls_db = {
+    "nodes":{},
+    "links":{},
+    "prefixes":{},
+}
 
 
 
-with open('ospf-sr.json') as sr:
-  data = json.load(sr)
+## Network OSPF json data
+with open(sys.argv[3]) as network_json:
+    network_data = json.load(network_json)
 
-print("Advertising node: " +data["srdbID"])
-for node in data["srNodes"]:
-	print(node["routerID"])
-	for entry in node["extendedLink"]:
-		print(entry)
+
+for area in network_data["networkLinkStates"]["areas"]:
+    for entry in network_data["networkLinkStates"]["areas"][area]:
+        
+
+        curr_net = {
+            "networkID":entry["linkStateId"],
+            "network":ipaddress.ip_interface("{}/{}".format(
+                entry["linkStateId"], 
+                entry["networkMask"])).network,
+            "rtrs":{},
+        }
+
+       
+
+        for i,link_a in enumerate(sorted(entry["attchedRouters"].keys())):
+            for j, link_b in enumerate( sorted(entry["attchedRouters"].keys()) ):
+                if (i != j):
+                    link_id  = "{}-{}".format(link_a, link_b)
+                    bgp_ls_db["links"][link_id] = {}
+                    bgp_ls_db["links"][link_id]["localRouter"] = link_a
+                    bgp_ls_db["links"][link_id]["neighborRouterId"] = link_b
+                    bgp_ls_db["links"][link_id]["routerLocalAddress"] = "UNKNOWN"
+                    bgp_ls_db["links"][link_id]["Adjacency SID"] = "UNKNOWN"
+                    bgp_ls_db["links"][link_id]["routerNeighborAddress"] = "UNKNOWN"
+
+
+
+
+# dotTemplate = Path('templates/dot.temp').read_text()
+# j2_dot = Template(dotTemplate, undefined=StrictUndefined)
+# topology["time"] = datetime.now().strftime("%c")
+# dotFile = j2_dot.render(topology)
+# # run dot with bash here
+# subprocess.call(
+#     "echo '{}' | sfdp -T jpg -o graph2.jpg".format(dotFile),
+#     shell=True
+# )
+
+## router OSPF json data
+with open(sys.argv[4]) as router_json:
+    router_data = json.load(router_json)
+
+
+for area in router_data["routerLinkStates"]["areas"]:
+    for router in router_data["routerLinkStates"]["areas"][area]:
+
+        bgp_ls_db["nodes"][router["linkStateId"]] = {
+            "rtr_ID":router["linkStateId"],
+            "OSPF_area":area,
+            "links":[]
+        }
+
+        for link in router["routerLinks"]:
+            if router["routerLinks"][link]["linkType"] == "another Router (point-to-point)":
+                # print(router["routerLinks"][link])
+
+                # links name from local and remote
+                link_id_l = "{}-{}".format(router["linkStateId"], router["routerLinks"][link]["neighborRouterId"])
+                link_id_r = "{}-{}".format(router["routerLinks"][link]["neighborRouterId"], router["linkStateId"])
+
+                # if this link from local exists before, its because it was defined as a remote
+                # therefore we know the remotes interface address
+                if link_id_l not in bgp_ls_db["links"].keys():
+                    # print("adding new local {}".format(link_id_l))
+                    bgp_ls_db["links"][link_id_l] = {}
+                    bgp_ls_db["links"][link_id_l]["routerNeighborAddress"] = "GARBAGE"
+                # else:
+                #     print("referencing old local {}".format(link_id_l))
+                
+                bgp_ls_db["links"][link_id_l]["localRouter"] = router["linkStateId"]
+                bgp_ls_db["links"][link_id_l]["neighborRouterId"] = router["routerLinks"][link]["neighborRouterId"]
+                bgp_ls_db["links"][link_id_l]["routerLocalAddress"] = router["routerLinks"][link]["routerInterfaceAddress"]
+                bgp_ls_db["links"][link_id_l]["Adjacency SID"] = "UNKNOWN"
+
+                if link_id_r not in bgp_ls_db["links"].keys():
+                    # print("adding new remote {}".format(link_id_r))
+                    bgp_ls_db["links"][link_id_r] = {}
+                # else:
+                #     print("referencing old remote {}".format(link_id_r))
+
+                bgp_ls_db["links"][link_id_r]["routerNeighborAddress"] = router["routerLinks"][link]["routerInterfaceAddress"]
+                    
+                
+                # not sure if should be listed as remote name or combined name
+                bgp_ls_db["nodes"][router["linkStateId"]]["links"].append(link_id_l)
+            elif router["routerLinks"][link]["linkType"] == "a Transit Network":
+                print(router["routerLinks"][link])
+                # bgp_ls_db["links"][link_id_l]["routerLocalAddress"] = router["routerLinks"][link]["routerInterfaceAddress"]
+
+                # routerInterfaceAddress
+
+
+
+## SR json data
+# with open(sys.argv[1]) as sr_json:
+#     SR_data = json.load(sr_json)
+
+# print("Advertising node: {}".format(SR_data["srdbID"]))
+# for node in SR_data["srNodes"]:
+#     SRGB_START = node["srgbLabel"]
+#     print(node["routerID"])
+#     # print (topology["routers"][node["routerID"]]["Transits"])
+#     # print(node)
+#     for entry in node["extendedPrefix"]:
+#         print("\tGLOBAL: {1}:{0}".format(entry['prefix'], entry['sid']+SRGB_START))
+#     for entry in node["extendedLink"]:
+#         print("\tLOCAL:  {1}:{0}".format(entry['prefix'], entry['sid']))
+
+#         remote_interface_address = str(ipaddress.ip_interface(entry['prefix']).ip)
+#         # topology["routers"][node["routerID"]]["Transits"][remote_interface_address]["label"] = entry['sid']
+
+
+# # print router information
+# print("Advertising node: {}".format(router_data["routerId"]))
+# for router_entry in topology["routers"]:
+#     print("\t""Router {}".format(router_entry))
+#     for interface in topology["routers"][router_entry]["Stubs"]:
+#         print("\t\t""Stub: {}".format(interface))
+#     for networkID in topology["routers"][router_entry]["Transits"]:
+        
+#         DR_address = topology["routers"][router_entry]["Transits"][networkID]["DR_address"]
+        
+#         print("\t\t""Transit: {}(local address)".format(
+#             ipaddress.ip_interface("{}/{}".format(
+#                 topology["routers"][router_entry]["Transits"][networkID]["address"],
+#                 topology["networks"][DR_address]["network"].netmask
+#             ))))
+        
+#         for router in topology["networks"][DR_address]["rtrs"]:
+#             if router != router_entry:
+#                 print("\t\t\t""Router: {}".format(router))
+#                 print("\t\t\t\t""Interface: {}".format(
+#                     topology["networks"][DR_address]["rtrs"][router]["interface"]
+#                     ))
+#                 print("\t\t\t\t""MPLS-Label: {}".format(
+#                     topology["routers"][router_entry]["Transits"][networkID]["label"]
+#                     ))
+#     print()
+
+
+
+# #print network information 
+# for networkID in topology["networks"]:
+#     print("\t""LinkID: {}".format(networkID))
+#     print("\t\t""Network: {}".format(topology["networks"][networkID]["network"]))
+#     for router in topology["networks"][networkID]["rtrs"]:
+#         print("\t\t""Interface: {} Router: {}".format(
+#                 topology["networks"][networkID]["rtrs"][router]["interface"], 
+#                 router ))
+
+
+#         # print("\t""Router {}".format(router))
+#         for interface in topology["routers"][router]["Stubs"]:
+#             print("\t\t\t""Stub: {}".format(interface))
+#         # for address in topology["routers"][router]["Transits"]:
+#         #     networkID = address
+#         #     print("\t\t\t""Transit: {} -- {}".format(
+#         #             topology["routers"][router]["Transits"][address]["address"],
+#         #             address))
+#         print()
+        
+#     print()
+
+
+# print("Nodes:")
+# for node in bgp_ls_db["nodes"]:
+#     # for attri in bgp_ls_db["nodes"][node]:
+#     #     print("\t\t{}: {}".format( attri, bgp_ls_db["nodes"][node][attri]))
+#     print("\t{}\n\t\tArea:{}\n\t\tLinks:{}".format(
+#         bgp_ls_db["nodes"][node]["rtr_ID"],
+#         bgp_ls_db["nodes"][node]["OSPF_area"],
+#         bgp_ls_db["nodes"][node]["links"],
+#         ))
+
+
+print("Links:")
+for link  in sorted(bgp_ls_db["links"].keys()):
+    print("\t{}\n\t\t{}|{} --> {}|{}\n\t\tSID:{}".format(
+        link,
+        bgp_ls_db["links"][link]["localRouter"],
+        bgp_ls_db["links"][link]["routerLocalAddress"],
+        bgp_ls_db["links"][link]["routerNeighborAddress"],
+        bgp_ls_db["links"][link]["neighborRouterId"],
+        bgp_ls_db["links"][link]["Adjacency SID"],
+        ))
--- tests/topotests/seg/test_seg.py
+++ tests/topotests/seg/test_seg.py
@@ -65,11 +65,12 @@ pytestmark = [
 
 
 
-networks = set()        # used to check if first or 2nd link to a network
+networks = {}        # used to check if first or 2nd link to a network
 
-link_list = []          # list of all rtr-rtr links
+network_dict = {}          # list of all rtr-rtr links
 routers_list = []       # list of all rtrs
 hosts_list = []       # list of all rtrs
+intf_dict = {} # intf_network_lookup["r1-eth0"] = 1
 
 class Router():
 
@@ -96,13 +97,24 @@ class Router():
         self.interfaces = []
         self.isRouter = 1 if rtr.name[0] == 'r' else 0
 
+        # print("router {} has {} interfaces".format(self.name, len(rtr.links)))
         for link in rtr.links:
             dev_n = link.split('eth')[1]
-            network = rtr.links[link][1][1:].split('-eth')[0]
-            subnet = 2 if network in networks else 1
-            networks.add(network)
-            self.interfaces.append(self.Interface(dev_n, network, subnet))
-        print("router {} has {} interfaces".format(self.name, len(self.interfaces)))
+            # network = rtr.links[link][1][1:].split('-eth')[0]
+            
+
+            # if network not in networks:
+            #     networks[network] = 1
+            # else:
+            #     networks[network] = networks[network] + 1 
+            # subnet = networks[network]
+
+            self.interfaces.append(self.Interface(
+                    dev_n, 
+                    intf_dict[link]["network_id"], 
+                    intf_dict[link]["subnet"]))
+            # print("\t {} - {}".format(link, intf_dict[link]["network_id"]))
+        
 
     def toDict(self):
         data ={
@@ -113,7 +125,7 @@ class Router():
         }
         if self.isRouter:
             data["gsrl"] = int(self.router_number)+4000
-        print(data)
+        # print(data)
 
         for interface in self.interfaces:
             data["interfaces"].append({
@@ -145,13 +157,24 @@ class SegTopo(Topo):
 
     
 
-    def produceRouterConfig(self, routers, zebraTemplate, ospfdTemplate):
+    def produceRouterConfig(self, data, zebraTemplate, ospfdTemplate):
 
-        for router in routers:
+        for router in data["routers"]:
+            # print (router["name"])
+            for interface in router["interfaces"]:
+                # print(interface)
+                # print ("\t{}".format(interface["network"]))
+                interface["network"] = {
+                    "id":interface["network"],
+                    "type":network_dict[interface["network"]]["type"],
+                }
+                
+
+
+
             j2_zebra = Template(zebraTemplate, undefined=StrictUndefined)
             zebraConf = j2_zebra.render(router)
-            
-           
+
 
             rtr_folder = 'config/{}'.format(router["name"])
             if not os.path.exists(rtr_folder):
@@ -169,56 +192,154 @@ class SegTopo(Topo):
                 with open(file, 'w') as filetowrite:
                     filetowrite.write(ospfdConf)
 
-    def produceHostsConfig(self, hosts, zebraTemplate):
+    # def produceHostsConfig(self, hosts, zebraTemplate):
 
-        for i, host in enumerate(hosts):
-            data ={
-                "name":host.name,
-                "rtr_n":i,
-                "interfaces":[]
-            }
-            for interface in host.interfaces:
-                data["interfaces"].append({
-                    "dev_n":interface.dev_n,
-                    "network":interface.network, 
-                    "subnet":interface.subnet,
-                })
-            j2_zebra = Template(zebraTemplate, undefined=StrictUndefined)
-            zebraConf = j2_zebra.render(host)            
+    #     for i, host in enumerate(hosts):
+    #         data ={
+    #             "name":host.name,
+    #             "rtr_n":i,
+    #             "interfaces":[]
+    #         }
+    #         for interface in host.interfaces:
+    #             data["interfaces"].append({
+    #                 "dev_n":interface.dev_n,
+    #                 "network":interface.network, 
+    #                 "subnet":interface.subnet,
+    #             })
+    #         j2_zebra = Template(zebraTemplate, undefined=StrictUndefined)
+    #         zebraConf = j2_zebra.render(host)            
 
-            rtr_folder = 'config/{}'.format(host["name"])
-            if not os.path.exists(rtr_folder):
-                os.makedirs(rtr_folder)
+    #         rtr_folder = 'config/{}'.format(host["name"])
+    #         if not os.path.exists(rtr_folder):
+    #             os.makedirs(rtr_folder)
 
-            file='{}/zebra.conf'.format(rtr_folder) 
-            with open(file, 'w') as filetowrite:
-                filetowrite.write(zebraConf)
+    #         file='{}/zebra.conf'.format(rtr_folder) 
+    #         with open(file, 'w') as filetowrite:
+    #             filetowrite.write(zebraConf)
 
     def produceGraph(self, data, dotTemplate):
 
         j2_dot = Template(dotTemplate, undefined=StrictUndefined)
         data["time"] = datetime.now().strftime("%c")
         dotFile = j2_dot.render(data)
+        print(dotFile)
         # run dot with bash here
         subprocess.call(
             "echo '{}' | sfdp -T jpg -o graph.jpg".format(dotFile),
             shell=True
         )
+        for link in data["links"]:
+            print(link)
+            print("{}".format(data["links"][link]))
 
 
 
-    def connect(self, r1, r2):
+    def connect_via_switch(self, router_list):
         tgen = get_topogen(self)
         self.networkCount = self.networkCount  + 1
-        switch = tgen.add_switch("s{}".format(self.networkCount))
-        switch.add_link(tgen.gears[r1])
-        switch.add_link(tgen.gears[r2])
 
-        link_list.append({
-            "src_rtr":r1,
-            "dst_rtr":r2,
-            "network":self.networkCount,
-            })
+        # only if addding switch
+        switch = tgen.add_switch("s{}".format(self.networkCount))
+
+        network_dict[self.networkCount] = {
+            "routers":router_list,
+            "network_id":self.networkCount,
+            "type":"broadcast",
+            }
+
+        subnet = 1
+
+        for router in network_dict[self.networkCount]["routers"]:
+
+            numLinks = len(tgen.gears[router].links)
+
+            # only if adding switch
+            switch.add_link(tgen.gears[router])
+
+            src_lnk = "{}-eth{}".format(router,numLinks)
+            dst_lnk = tgen.gears[router].links[src_lnk][1]
+
+
+            intf_dict[src_lnk] = { 
+                    "network_id":self.networkCount,
+                    "subnet":subnet,
+            }
+            subnet = subnet + 1
+            # intf_dict[dst_lnk] = network_dict[self.networkCount]
+            print ("Connecting {} to {} via network {}:{}".format(
+                    src_lnk, 
+                    dst_lnk,
+                    network_dict[self.networkCount]["network_id"],
+                    network_dict[self.networkCount]["type"],
+                    ))
+
+    def ptp_connect(self, r1, r2):
+        tgen = get_topogen(self)
+
+        self.networkCount = self.networkCount + 1
+
+        network_dict[self.networkCount] = {
+            "routers":[r1,r2],
+            "network_id":self.networkCount,
+            "type":"point-to-point",
+            }
+
+        numLinks = len(tgen.gears[r1].links)
+        tgen.gears[r1].add_link(tgen.gears[r2])
+
+        src_lnk = "{}-eth{}".format(r1,numLinks)
+        dst_lnk = tgen.gears[r1].links[src_lnk][1]
+
+        intf_dict[src_lnk] = { 
+            "network_id":self.networkCount,
+            "subnet":1,
+        }
+        intf_dict[dst_lnk] = { 
+            "network_id":self.networkCount,
+            "subnet":2,
+        }
+
+        print ("Connecting {} to {} via network {}:{}".format(
+                    src_lnk, 
+                    dst_lnk,
+                    network_dict[self.networkCount]["network_id"],
+                    network_dict[self.networkCount]["type"],
+                    ))
+
+
+    def attach_host(self, rtr, host):
+        tgen = get_topogen(self)
+
+        self.networkCount = self.networkCount + 1
+
+        network_dict[self.networkCount] = {
+            "routers":[rtr, host],
+            "network_id":self.networkCount,
+            "type":"broadcast",
+            }
+
+        tgen.add_router(host)
+        numLinks = len(tgen.gears[rtr].links)
+        tgen.gears[rtr].add_link(tgen.gears[host])
+
+        src_lnk = "{}-eth{}".format(rtr,numLinks)
+        dst_lnk = tgen.gears[rtr].links[src_lnk][1]
+        intf_dict[src_lnk] = { 
+            "network_id":self.networkCount,
+            "subnet":1,
+        }
+        intf_dict[dst_lnk] = { 
+            "network_id":self.networkCount,
+            "subnet":2,
+        }
+
+        print ("Connecting {} to {} via network {}:{}".format(
+                src_lnk, 
+                dst_lnk,
+                network_dict[self.networkCount]["network_id"],
+                network_dict[self.networkCount]["type"],
+                ))
+
 
 
     def build(self, *_args, **_opts):
@@ -228,51 +349,60 @@ class SegTopo(Topo):
         for routern in range(1, 13):
             tgen.add_router("r{}".format(routern))
 
-        # create a host computer attached to r1 
-        tgen.add_router("h1")
-        self.connect("h1", "r1")
+        self.attach_host("r1", "h1")
+        self.attach_host("r12", "h2")
+        # self.attach_host("r11", "h3")
 
-        tgen.add_router("h2")
-        self.connect("h2", "r11")
+        tgen.add_router("r13")
+        self.connect_via_switch(["r1", "r2", "r13"])
+        # self.ptp_connect("r1", "r2")
 
-        self.connect("r1",   "r2")
-        self.connect("r1",   "r4")
-        self.connect("r2",   "r3")
-        self.connect("r2",   "r4")
-        self.connect("r2",   "r6")
-        self.connect("r3",   "r4")
-        self.connect("r3",   "r5")
-        self.connect("r5",   "r6")
-        self.connect("r5",   "r8")
-        self.connect("r5",   "r7")
-        self.connect("r6",   "r7")
-        self.connect("r6",   "r8")
-        self.connect("r7",   "r8")
-        self.connect("r7",  "r10")
-        self.connect("r8",   "r9")
-        self.connect("r9",  "r10")
-        self.connect("r9",  "r12")
-        self.connect("r10", "r11")
-        self.connect("r10", "r12")
-        self.connect("r11", "r12")
+        self.ptp_connect("r1", "r4")
+        self.ptp_connect("r2",   "r3")
+        self.ptp_connect("r2",   "r4")
+        self.ptp_connect("r2",   "r6")
+        self.ptp_connect("r3",   "r4")
+        self.ptp_connect("r3",   "r5")
+        self.ptp_connect("r5",   "r6")
+        self.ptp_connect("r5",   "r8")
+
+        self.ptp_connect("r5", "r7")
+        self.ptp_connect("r6", "r8")
+
+        self.ptp_connect("r6",   "r7")
+        
+        self.ptp_connect("r7",   "r8")
+        self.ptp_connect("r7",  "r10")
+        self.ptp_connect("r8",   "r9")
+        self.ptp_connect("r9",  "r10")
+        self.ptp_connect("r9",  "r12")
+
+        tgen.add_router("r14")
+        self.connect_via_switch(["r10", "r11", "r14"])
+        # self.ptp_connect("r10", "r11")
+
+        self.ptp_connect("r10", "r12")
+        self.ptp_connect("r11", "r12")
+
 
 
         zebraTemplate = Path('templates/zebra.temp').read_text()
         ospfdTemplate = Path('templates/ospfd.temp').read_text()
         dotTemplate = Path('templates/dot.temp').read_text()
+
         # For all registred routers, load the zebra configuration file
         for rname, router in tgen.routers().items():
-
             router_dict = Router(get_topogen(self).gears[rname]).toDict()
             routers_list.append(router_dict)
 
-        self.produceRouterConfig(routers_list, zebraTemplate, ospfdTemplate)
 
         data = {
             "hosts":hosts_list,
             "routers":routers_list,
-            "links":link_list,
+            "links":network_dict,
         }
+
+        self.produceRouterConfig(data, zebraTemplate, ospfdTemplate)
 
         self.produceGraph(data, dotTemplate)
         print("Finished producing graph for topology")
@@ -280,8 +410,11 @@ class SegTopo(Topo):
 
 def setup_module(mod):
     "Sets up the pytest environment"
-    # This function initiates the topology build with Topogen...
-    tgen = Topogen(SegTopo, mod.__name__)
+    # This function initiates the topology build with Topogen....
+    if mod == None:
+        tgen = Topogen(SegTopo, "standalone")
+    else:
+        tgen = Topogen(SegTopo, mod.__name__)
     # ... and here it calls Mininet initialization functions.
     tgen.start_topology()
 
@@ -323,34 +456,11 @@ def test_call_mininet_cli():
     tgen.mininet_cli()
 
 
-# def test_ospf_sr():
-#     "Test OSPF daemon Segment Routing"
-#     tgen = get_topogen()
-#     if tgen.routers_have_failure():
-#         pytest.skip(tgen.errors)
-
-#     logger.info("--- test OSPF Segment Routing Data Base ---")
-
-#     for rnum in range(1, 5):
-#         router = "r{}".format(rnum)
-
-#         logger.info('\tRouter "%s"', router)
-
-#         # Load expected results from the command
-#         reffile = os.path.join(CWD, "{}/ospf_srdb.json".format(router))
-#         expected = json.loads(open(reffile).read())
-
-#         # Run test function until we get an result. Wait at most 60 seconds.
-#         rt = tgen.gears[router]
-#         test_func = partial(
-#             topotest.router_json_cmp,
-#             rt,
-#             "show ip ospf database segment-routing json",
-#             expected,
-#         )
-#         rv, diff = topotest.run_and_expect(test_func, None, count=25, wait=3)
-#         assert rv, "OSPF did not start Segment Routing on {}:\n{}".format(router, diff)
-
 if __name__ == "__main__":
     args = ["-s"] + sys.argv[1:]
-    sys.exit(pytest.main(args))
+    if len(sys.argv) > 1 and sys.argv[1] == "debug":
+        setup_module(None)
+        teardown_module(None)
+    else:
+        sys.exit(pytest.main(args))
+
